07ad4f54300307b34d19bf2033d2bc94532a598a
olegbb1502 <bolobanoleg@gmail.com>
2018-07-24 16:53:28 +0300

    finish desktop

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 var number = document.getElementById('counter');
-var more = document.getElementsByClassName('more');
-var less = document.getElementsByClassName('less');
 
 var value = number.getAttribute("value");
 number.setAttribute("value", 1+' page');
 
-console.log(value);
+// console.log(value);
 
 function add() {
     value++;
-    console.log(value);
+    // console.log(value);
     number.setAttribute("value", value+' pages');
 }
 
 function subtract() {
     if(value>1){
         value--;
-        console.log(value);
+        // console.log(value);
         number.setAttribute("value", value+' pages');
     }
     else number.setAttribute("value", 1+' page');
@@ -27,12 +25,12 @@ function countPrice() {
     var type = document.getElementById('type').value;
     var place = document.getElementById('place').value;
     var days = document.getElementById('days').value;
-    console.log(type);
-    console.log(place);
-    console.log(days);
-    console.log(value);
+    // console.log(type);
+    // console.log(place);
+    // console.log(days);
+    // console.log(value);
     if(type == "Essay" && place == "University" && days == "14 days" && value == "1"){
-        console.log('success');
+        // console.log('success');
         summ.innerHTML = "12";
         document.getElementById('error').style.display = 'none';
 
